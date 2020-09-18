2424f7dc6afa5745955671eed193a966fb70da48
Vincent Pham <46042000+lopsided-apples@users.noreply.github.com>
2020-09-17 18:03:31 -0700

    Add files via upload

---
+++ backend/data.js
@@ -0,0 +1,103 @@
+export default {
+  products: [ 
+  {
+    _id: '1',
+    name: 'Active Shorts',
+    category: 'Mens',
+    image: '/images/d1.jpg',
+    price: 30,
+    brand: 'Alphalete',
+    rating: 4.5,
+    numReviews: 89,
+    countInStock: 10
+  },
+  {
+    _id: '2',
+    name: 'Force Joggers',
+    category: 'Mens',
+    image: '/images/d2.jpg',
+    price: 50,
+    brand: 'GYMSHARK',
+    rating: 4.8,
+    numReviews: 165,
+    countInStock: 18
+  },
+  {
+    _id: '3',
+    name: 'Engineered Warmth Jacket',
+    category: 'Mens',
+    image: '/images/d3.jpg',
+    price: 150,
+    brand: 'lululemon',
+    rating: 4.7,
+    numReviews: 200,
+    countInStock: 5
+  },
+  {
+    _id: '4',
+    name: 'Power Down Shorts',
+    category: 'Womens',
+    image: '/images/d4.jpg',
+    price: 40,
+    brand: 'GYMSHARK',
+    rating: 4.4,
+    numReviews: 53,
+    countInStock: 28
+  },
+  {
+    _id: '5',
+    name: 'OG Revival Leggings',
+    category: 'Womens',
+    image: '/images/d5.jpg',
+    price: 60,
+    brand: 'Alphalete',
+    rating: 4.8,
+    numReviews: 167,
+    countInStock: 9
+  },
+  {
+    _id: '6',
+    name: 'Cates Tee',
+    category: 'Womens',
+    image: '/images/d6.jpg',
+    price: 35,
+    brand: 'lululemon',
+    rating: 4.3,
+    numReviews: 5,
+    countInStock: 33
+  },
+  {
+    _id: '7',
+    name: 'GRID Foam Roller',
+    category: 'Accessories',
+    image: '/images/d7.jpg',
+    price: 20,
+    brand: 'Trigger Point Performance',
+    rating: 4.9,
+    numReviews: 312,
+    countInStock: 59
+  },
+  {
+    _id: '8',
+    name: 'Yoga Mat',
+    category: 'Accessories',
+    image: '/images/d8.jpg',
+    price: 90,
+    brand: 'manduka',
+    rating: 4.7,
+    numReviews: 175,
+    countInStock: 3
+  },
+  {
+    _id: '9',
+    name: 'Genuine Leather Weightlifting Belt',
+    category: 'Accessories',
+    image: '/images/d9.jpg',
+    price: 45,
+    brand: 'Dark Iron Fitness',
+    rating: 4.6,
+    numReviews: 56,
+    countInStock: 42
+  }
+  ]
+}
